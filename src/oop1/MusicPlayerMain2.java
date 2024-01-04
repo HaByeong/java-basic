148f6b2cff1d951a487044882e2d9bb49ff3c45f
HaByeong <121917642+HaByeong@users.noreply.github.com>
2024-01-04 23:58:43 +0900

    절차 지향 모듈화

--- src/oop1/MusicPlayerMain2.java
+++ src/oop1/MusicPlayerMain2.java
@@ -6,29 +6,44 @@ public class MusicPlayerMain2 {
         MusicPlayerData data = new MusicPlayerData();
 
         //음악 플레이어 켜기
+        on(data);
+        //볼륨 증가
+        volumeUp(data);
+        //볼륨 증가
+        volumeUp(data);
+        //볼륨 감소
+        volumeDown(data);
+        //음악 플레이어 상태
+        showStatus(data);
+        //음악 플레이어 끄기
+        off(data);
+    }
+
+    static void on(MusicPlayerData data) {
         data.isOn = true;
         System.out.println("음악 플레이어를 시작합니다");
+    }
+    static void off(MusicPlayerData data) {
+        data.isOn = false;
+        System.out.println("음악 플레이어를 종료합니다");
+    }
 
-        //볼륨 증가
+    static void volumeUp(MusicPlayerData data) {
         data.volume++;
         System.out.println("음악 플레이어 볼륨:" + data.volume);
-        //볼륨 증가
-        data.volume++;
-        System.out.println("음악 플레이어 볼륨:" + data.volume);
-        //볼륨 감소
+    }
+
+    static void volumeDown(MusicPlayerData data) {
         data.volume--;
         System.out.println("음악 플레이어 볼륨:" + data.volume);
+    }
 
-        //음악 플레이어 상태
+    static void showStatus(MusicPlayerData data) {
         System.out.println("음악 플레이어 상태 확인");
         if (data.isOn) {
             System.out.println("음악 플레이어 ON, 볼륨:" + data.volume);
         } else {
             System.out.println("음악 플레이어 OFF");
         }
-
-        //음악 플레이어 끄기
-        data.isOn = false;
-        System.out.println("음악 플레이어를 종료합니다");
     }
 }
