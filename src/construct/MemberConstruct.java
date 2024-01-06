75ef982cf5f76a4b64223115f4e9ba0aebf9a6e5
HaByeong <121917642+HaByeong@users.noreply.github.com>
2024-01-06 17:15:12 +0900

    오버로딩과 this()

--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -5,6 +5,10 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    //추가
+    MemberConstruct(String name, int age) {
+        this(name, age, 50);
+    }
     MemberConstruct(String name, int age, int grade) {
         System.out.println("생성자 호출 name =" + name + ",age=" + age + ".grade=" + grade);
         this.name = name;
