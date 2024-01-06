3caa343110c13eba77da9d85527b61f5f3aa932f
HaByeong <121917642+HaByeong@users.noreply.github.com>
2024-01-06 18:02:55 +0900

    패키지 - import

--- src/pack/PackageMain2.java
+++ src/pack/PackageMain2.java
@@ -1,11 +1,14 @@
 package pack;
 
-import pack.a.User;
+//import pack.a.User;
+//import pack.a.User2;
+import pack.a.*;
 
-public class PackageMain1 {
+public class PackageMain2 {
 
     public static void main(String[] args) {
         Data data = new Data();
         User user = new User();
+        User2 user2 = new User2();
     }
 }
---
+++ src/pack/a/User2.java
@@ -0,0 +1,7 @@
+package pack.a;
+
+public class User2 {
+    public User2() {
+        System.out.println("pack.a.User2 생성");
+    }
+}
