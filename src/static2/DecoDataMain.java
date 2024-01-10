7550a2a4cca30c1371ecf96ef4d1f20e408336f5
HaByeong <121917642+HaByeong@users.noreply.github.com>
2024-01-10 09:58:11 +0900

    static 메서드3

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,9 +1,14 @@
 package static2;
 
+//import static static2.DacoData.staticCall;
 public class DecoDataMain {
 
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
+        DacoData.staticCall();
+        DacoData.staticCall();
+        DacoData.staticCall();
+        DacoData.staticCall();
         DacoData.staticCall();
 
         System.out.println("2. 인스턴스 호출1");
@@ -11,5 +16,13 @@ public class DecoDataMain {
         data1.instanceCall();
 
         DacoData.staticCall(data1);
+
+        //추가
+        //인스턴스를 통한 접근
+        DacoData data3 = new DacoData();
+        data3.staticCall();
+
+        //클래스 통한 접근
+        DacoData.staticCall();
     }
 }
