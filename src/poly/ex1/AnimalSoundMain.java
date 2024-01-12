d59c72d2649dcf89a9e5cbd992768d0c31b1827d
HaByeong <121917642+HaByeong@users.noreply.github.com>
2024-01-12 17:29:36 +0900

    다형성 활용2

--- src/poly/ex1/AnimalSoundMain.java
+++ src/poly/ex1/AnimalSoundMain.java
@@ -6,7 +6,7 @@ public class AnimalSoundMain {
         Cat cat = new Cat();
         Caw caw = new Caw();
 
-        Caw[] cawArr = {dog, cat, caw};
+        //Caw[] cawArr = {dog, cat, caw};
         System.out.println("동물 소리 테스트 시작");
         dog.sound();
         System.out.println("동물 소리 테스트 종료");
---
+++ src/poly/ex2/AnimalPolyMain1.java
@@ -0,0 +1,23 @@
+package poly.ex2;
+
+public class AnimalPolyMain1 {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Caw caw = new Caw();
+        Duck duck = new Duck();
+
+        soundAnimal(dog);
+        soundAnimal(cat);
+        soundAnimal(caw);
+        soundAnimal(duck);
+    }
+
+    private static void soundAnimal(Animal animal) {
+        System.out.println("동물 소리 테스트 시작");
+        //메서드 오버라이딩을 이용!!
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+}
