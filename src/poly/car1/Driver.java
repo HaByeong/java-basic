0bf5f7b58416e73bdbf6f897800314c8b6c9ed26
HaByeong <121917642+HaByeong@users.noreply.github.com>
2024-01-14 17:44:41 +0900

    다형성 - 역할과 구현 예제3

---
+++ src/poly/car1/Driver.java
@@ -0,0 +1,17 @@
+package poly.car1;
+
+public class Driver {
+    private Car car; //자동차라는 역할만 안다.
+
+    public void setCar(Car car) {
+        System.out.println("자동차 설정: " + car);
+        this.car = car;
+    }
+
+    public void drive() {
+        System.out.println("자동차를 운전합니다.");
+        car.startEngine();
+        car.pressAccelerator();
+        car.offEngine();
+    }
+}
