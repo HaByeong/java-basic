73eaf1bf3e2c5b14577ae98b86e969a48dced31a
HaByeong <121917642+HaByeong@users.noreply.github.com>
2024-01-15 01:35:02 +0900

    다형성과 설계 문제 풀이

---
+++ src/poly/ex/pay0/PayService.java
@@ -0,0 +1,22 @@
+package poly.ex.pay0;
+
+public class PayService {
+    public void processPay(String option, int amount) {
+        boolean result;
+        System.out.println("결제를 시작합니다: option=" + option + ", amount=" + amount);
+        if (option.equals("kakao")) {
+            KakaoPay pay = new KakaoPay();
+            result = pay.pay(amount);
+        } else if (option.equals("naver")) {
+            NaverPay pay = new NaverPay();
+            result = pay.pay(amount);
+        } else {
+            System.out.println("결제 수단이 없습니다."); result = false;
+        }
+        if (result) {
+            System.out.println("결제가 성공했습니다.");
+        } else {
+            System.out.println("결제가 실패했습니다.");
+        }
+    }
+}
