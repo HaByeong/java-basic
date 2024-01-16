feb109b775befebc933c0f62541f1599a5c5e3d1
HaByeong <121917642+HaByeong@users.noreply.github.com>
2024-01-16 20:17:19 +0900

    ArrayListAdd

---
+++ src/arraylist/ArrayListAdd.java
@@ -0,0 +1,39 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListAdd {
+    public static void main(String[] args) {
+        // 기본 길이(용량, capacity)가 10인 ArrayList를 생성
+        ArrayList<Integer> list1 = new ArrayList<>(10);
+        // ArrayList에는 객체만 저장이 가능하다.
+        // autoboxing에 의해 기본형이 참조형으로 자동 변환
+        list1.add(5); // 실제로는 컴파일러가 list1.add(new Integer(5)); 로 바꿔줌
+        list1.add(4); // 근데 나는 자바 버전이 높아서 제네릭 <> 이거 써줘서 해결
+        list1.add(2);
+        list1.add(0);
+        list1.add(1);
+        list1.add(3);
+
+        ArrayList<Integer> list2 = new ArrayList<>(list1.subList(1,4));
+
+        System.out.println(list1);
+        System.out.println(list2);
+
+        //Collection은 인터페이스, Collections는 유틸 클래스
+        Collections.sort(list1); //list1과 list2를 정렬한다.
+        Collections.sort(list2);
+        System.out.println(list1);
+        System.out.println(list2);
+
+        //list1이 list2를 모두 가지고 있는지 확인 -> 맞으면 true 반환
+        System.out.println("list1.containsAll(list2): " + list1.containsAll(list2));
+
+        list2.add("B");
+        list2.add("C");
+        list2.add(3, "A");
+        System.out.println(list1);
+        System.out.println(list2);
+    }
+}
